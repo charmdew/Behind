5a06fe7ba64f2e5dcea91a709821cea59015e11b
YouYoungLee <dldbdud111@gmail.com>
2023-01-30 13:21:34 +0900

    check

--- behind-back/src/main/java/com/reboot/behind/data/entity/User.java
+++ behind-back/src/main/java/com/reboot/behind/data/entity/User.java
@@ -1,17 +1,11 @@
 package com.reboot.behind.data.entity;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -21,7 +15,8 @@ import java.util.stream.Collectors;
 @Builder
 @Table
 @Entity
-public class User implements UserDetails {
+//implements UserDetails
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -29,16 +24,16 @@ public class User implements UserDetails {
     @Column(nullable = false, unique = true)
     private String userId;
 
-    @JsonProperty(access = Access.WRITE_ONLY) //Json으로 결과가 출력되지 않도록 어노테이션 설정 값을 추가하였다.
+//    @JsonProperty(access = Access.WRITE_ONLY) //Json으로 결과가 출력되지 않도록 어노테이션 설정 값을 추가하였다.
     @Column(nullable = false)
     private String password;
 
     @Column(nullable = false)
     private String name;
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @Builder.Default
-    private List<String> roles = new ArrayList<>();
+//    @ElementCollection(fetch = FetchType.EAGER)
+//    @Builder.Default
+//    private List<String> roles = new ArrayList<>();
 
     @Column(nullable = false)
     private String email;
@@ -48,13 +43,11 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private int position2;
 
-    @Column(nullable = false)
     @ElementCollection
-    private List<String> tag;
+    @Column(nullable = false)
+    private List<String> tag ;
 
     private String phoneNum;
-
-    private boolean isVisible;
 
     @Column(nullable = false)
     private int track1;
@@ -70,44 +63,45 @@ public class User implements UserDetails {
 
     @ElementCollection
     private List<Integer> followUsers;
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
 
-        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-    }
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//
+//        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+//    }
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Override
-    public String getUsername() {
-        return this.userId;
-    }
+//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+//    @Override
+//    public String getUsername() {
+//        return this.userId;
+//    }
 
     //계정 만료 체크하는 로직. 사용하지 않으므로 true
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
+//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return false;
+//    }
 
 
     //계정이 잠겼는지 체크하는 로직. 현재 사용하지 않으므로 true로 두었다.
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
+//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return false;
+//    }
 
     //패스워드 만료 체크 로직. 사용하지 않으므로 true;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
 
     //계정이 사용가능한지 체크하는 로직이다. 현재 계정 정지 기능을 사용하지 않으므로 true로 두었다.
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
 }
