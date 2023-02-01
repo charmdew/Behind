ee28e3164fb9eca55f673142bc3f82f65ed4003c
RyuJeongmin <jmyoo88@naver.com>
2023-02-01 17:50:38 +0900

    Feat: 이미지를 여러 스타일로 변환하는 기능 구현

--- behind-ai/behind-flask/app.py
+++ behind-ai/behind-flask/app.py
@@ -43,5 +43,36 @@ def nst_test():
     return json.dumps({"[Success] Total time": time})
 
 
+# 여러 스타일을 적용하여 이미지 변환
+@app.route('/nst/multiple', methods=['POST'])
+def nst_multiple():
+    # 파일 받기
+    content_file = request.files['file']
+
+    # 변환할 이미지
+    content_image = Image.open(content_file.stream)
+
+    ##### 결과 이미지 데이터 #####
+    # 변환된 이미지
+    b64encoded_images = neural_style_transfer.main_multiple_styles(content_image, content_file.filename)
+    # base64로 인코딩된 문자열을 디코딩 
+    styled_image_data = [b64encoded.decode("utf-8") for b64encoded in b64encoded_images]
+
+    # 이미지 데이터를 JSON으로 응답하는 경우
+    return json.dumps({"images": styled_image_data})
+    # return jsonify({
+    #     'msg': 'success',
+    #     'size': content_image.size,
+    #     'format': 'JPEG',
+    #     'image': styled_image_data
+    # })
+
+    # ##### 걸린 시간 (테스트용) #####
+    # # 변환하는데 걸린 시간 반환 (테스트용)
+    # time = neural_style_transfer.main_multiple_styles(content_image, content_file.filename)
+    #
+    # return json.dumps({"[Success] Total time": time})
+
+
 if __name__ == '__main__':
     app.run()
