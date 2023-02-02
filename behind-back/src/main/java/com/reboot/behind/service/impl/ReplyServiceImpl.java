15d95de6cd5b9762663f5ddd56ec8cccd6b69e85
이상훈 <akqjqdmltn@naver.com>
2023-02-02 14:50:10 +0900

    ready to rebase

--- behind-back/src/main/java/com/reboot/behind/service/impl/ReplyServiceImpl.java
+++ behind-back/src/main/java/com/reboot/behind/service/impl/ReplyServiceImpl.java
@@ -34,7 +34,7 @@ public class ReplyServiceImpl implements ReplyService {
         Comment comment = commentRepository.findById(replyDto.getCommentId()).get();
 
         reply.setContent(replyDto.getContent());
-        reply.setComment(comment);
+//        reply.setComment(comment);
         reply.setWriterId(user);
         reply.setCreatedTime(LocalDateTime.now());
         Reply saveReply = replyRepository.save(reply);
@@ -59,7 +59,7 @@ public class ReplyServiceImpl implements ReplyService {
         replyResponseDto.setReplyId(changedReply.getReplyId());
         replyResponseDto.setContent(changedReply.getContent());
         replyResponseDto.setWriterName(changedReply.getWriterId().getName());
-        replyResponseDto.setCommentId(changedReply.getComment().getCommentId());
+//        replyResponseDto.setCommentId(changedReply.getComment().getCommentId());
         replyResponseDto.setCreateTime(changedReply.getCreatedTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         replyResponseDto.setUpdateTime(changedReply.getUpdatedTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
 
