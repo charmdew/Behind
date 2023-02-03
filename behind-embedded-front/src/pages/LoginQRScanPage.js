acae15c1a8b875bfcac22c2c16cc4f1b1fb79223
Joonha Yun <neverdcloud@gmail.com>
2023-02-03 09:31:37 +0900

    update login qr page

--- behind-embedded-front/src/pages/LoginQRScanPage.js
+++ behind-embedded-front/src/pages/LoginQRScanPage.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Box } from '@chakra-ui/react'
+import { QrScanner } from '@yudiel/react-qr-scanner'
 
 const LoginQRScanPage = () => {
-  return (<div>LoginQRScanPage</div>)
+  const containerStyle = {
+    width: '100vw',
+    height: '100vh'
+  }
+
+  return (
+    <Box w="100vw" h="100vh" bgColor="black">
+      <Box w="100vh" m="auto">
+        <QrScanner
+          onDecode={(result) => console.log(result)}
+          onError={(error) => console.log(error?.message)}
+        />
+      </Box>
+    </Box>
+  )
 }
 
 export default LoginQRScanPage
