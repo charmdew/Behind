cddc1d771a10e10e0b61c6fbcdf1ca0aabb99a05
Joonha Yun <neverdcloud@gmail.com>
2023-02-07 19:17:07 +0900

    modify key press mechanism

--- behind-embedded-front/src/pages/StartPage.js
+++ behind-embedded-front/src/pages/StartPage.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Box, Text, Flex } from '@chakra-ui/react'
 
 const StartPage = () => {
   const navigate = useNavigate()
 
-  window.addEventListener(
-    'keydown',
-    (e) => {
-      if (e.key === 'Enter') navigate('/login-guide')
-    },
-    true
-  )
+  const keyDownHandler = (e) => {
+    if (e.key === 'Enter') navigate('/login-guide')
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDownHandler)
+
+    return () => {
+      window.removeEventListener('keydown', keyDownHandler)
+    }
+  }, [])
 
   return (
     <Box w="100vw" h="100vh" p="5%" bgColor="teal">
       <Flex w="100%" h="100%" direction="column" justify="center">
-        <Text w="100%" textAlign="center" fontSize="9xl" color="white">
+        <Text textAlign="center" fontSize="9xl" color="white">
           Behind
         </Text>
         <Text textAlign="center" fontSize="6xl" color="white">
