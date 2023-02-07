1578295efbb7b4b931c49430480882c75c1796bf
Joonha Yun <neverdcloud@gmail.com>
2023-02-07 19:17:40 +0900

    add photo shoot feature

--- behind-embedded-front/src/pages/PhotoShootGuidePage.js
+++ behind-embedded-front/src/pages/PhotoShootGuidePage.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import Layout from '../components/Layout'
+import photoShootGuideImageTemp from '../assets/photo-shoot-guide-image-temp.png'
 
 const PhotoShootGuidePage = () => {
-  return <div>PhotoShootGuidePage</div>
+  const navigate = useNavigate()
+
+  const keyDownHandler = (e) => {
+    // temp start
+    if (e.key === 'ArrowLeft') navigate('/menu')
+    // temp end
+    if (e.key === 'Enter') navigate('/photo-shoot')
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDownHandler)
+
+    return () => {
+      window.removeEventListener('keydown', keyDownHandler)
+    }
+  }, [])
+
+  return (
+    <Layout
+      heading="로그인"
+      body={
+        <>
+          가운데 프레임에 맞추어
+          <br />
+          사진을 촬영해주세요.
+        </>
+      }
+      imageSrc={photoShootGuideImageTemp}
+      imageAlt="Photo shoot guide"
+      isButton={true}
+    />
+  )
 }
 
 export default PhotoShootGuidePage
--- behind-embedded-front/src/pages/PhotoShootPage.js
+++ behind-embedded-front/src/pages/PhotoShootPage.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useEffect, useRef, useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Webcam from 'react-webcam'
+import { Box } from '@chakra-ui/react'
 
-const PhotoShootPage = () => {
-  return <div>PhotoShootPage</div>
+// Temp start
+// frame needed
+// Temp
+
+const PhotoShootPage = ({ setImageSrc }) => {
+  const navigate = useNavigate()
+  const webcamRef = useRef(null)
+  const capture = useCallback(() => {
+    return webcamRef.current.getScreenshot()
+  }, [webcamRef])
+
+  const keyDownHandler = (e) => {
+    if (e.key === 'Enter') {
+      setImageSrc(capture())
+      navigate('/after-shoot')
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDownHandler)
+
+    return () => {
+      window.removeEventListener('keydown', keyDownHandler)
+    }
+  }, [])
+
+  const videoConstraints = {
+    width: { min: 640, ideal: 1920 },
+    height: { min: 400, ideal: 1080 },
+    aspectRatio: { ideal: 1.7777777778 }
+  }
+  return (
+    <Box bgColor="black">
+      <Webcam
+        ref={webcamRef}
+        style={{ height: '100vh' }}
+        width={1920}
+        height={1080}
+        screenshotFormat="image/jpeg"
+        videoConstraints={videoConstraints}
+      />
+    </Box>
+  )
 }
 
 export default PhotoShootPage
