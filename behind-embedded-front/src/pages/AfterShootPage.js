444478f4f936622017008bee7247c7ca209c6a81
Joonha Yun <neverdcloud@gmail.com>
2023-02-11 17:40:46 +0900

    modify after shoot page

--- behind-embedded-front/src/pages/AfterShootPage.js
+++ behind-embedded-front/src/pages/AfterShootPage.js
@@ -1,116 +1,70 @@
 import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Box, Flex } from '@chakra-ui/react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { Center, Flex, Image } from '@chakra-ui/react'
+import { HiArrowCircleLeft } from 'react-icons/hi'
+import { GiButtonFinger } from 'react-icons/gi'
 
-import Layout from '../components/Layout'
-import ProfileCardPhoto from '../components/ProfileCardPhoto'
+import Container from '../components/Container'
+import Header from '../components/Header'
+import ButtonLg from '../components/ButtonLg'
+import IconWithLabel from '../components/IconWithLabel'
+import cropImageToFitProfileCard from '../utils/cropImageToFitProfileCard'
 
-const AfterShootPage = ({ imageSrc }) => {
+const AfterShootPage = () => {
+  const { state } = useLocation()
   const navigate = useNavigate()
   const [selection, setSelection] = useState('up')
+  const [croppedCaptureDataURL, setCroppedCaptureDataURL] = useState(null)
+
+  cropImageToFitProfileCard(state.captureDataURL, setCroppedCaptureDataURL)
 
   const keyDownHandler = (e) => {
-    if (e.key === 'ArrowUp') {
-      setSelection('down')
-      console.log(selection)
-    }
-    if (e.key === 'ArrowDown') {
-      setSelection('down')
-      console.log(selection)
-    }
+    if (e.key === 'ArrowLeft')
+      navigate('/reset', { state: { prevPage: '/after-shoot', ...state } })
+    if (e.key === 'ArrowUp') setSelection('up')
+    if (e.key === 'ArrowDown') setSelection('down')
     if (e.key === 'Enter') {
-      console.log(selection)
+      if (selection === 'up') {
+        const { captureDataURL, ...newState } = state
+        navigate('/photo-transform', {
+          state: { croppedCaptureDataURL: croppedCaptureDataURL, ...newState }
+        })
+      }
+      if (selection === 'down') {
+        const { captureDataURL, ...newState } = state
+        navigate('/photo-shoot', { state: newState })
+      }
     }
   }
 
   useEffect(() => {
     window.addEventListener('keydown', keyDownHandler)
+    return () => window.removeEventListener('keydown', keyDownHandler)
+  }, [selection, croppedCaptureDataURL])
 
-    return () => {
-      window.removeEventListener('keydown', keyDownHandler)
-    }
-  }, [])
-
-  imageHandler(imageSrc)
-
-  const body = (
-    <Box>
-      <Flex direction="row" align="center" gap="10vw">
-        <Flex direction="column" gap="5vh">
-          <Box
-            px="4vw"
-            py="4vh"
-            color={selection === 'up' ? 'white' : 'black'}
-            bgColor={selection === 'up' ? 'teal' : 'gray'}
-            fontSize="4vw"
-            borderRadius="lg">
-            다음
-          </Box>
-          <Box
-            px="4vw"
-            py="4vh"
-            color={selection === 'down' ? 'white' : 'black'}
-            bgColor={selection === 'down' ? 'teal' : 'gray'}
-            fontSize="4vw"
-            borderRadius="lg">
-            다시 찍기
-          </Box>
+  return (
+    <Center w="100vw" h="100vh">
+      <Container>
+        <Header>촬영 완료</Header>
+        <Flex as="main" direction="row" justify="center" gap="5vw">
+          <Flex direction="column" justify="center" gap="3vw">
+            <ButtonLg isSelected={selection === 'up'}>다음</ButtonLg>
+            <ButtonLg isSelected={selection === 'down'}>다시 찍기</ButtonLg>
+          </Flex>
+          <Image
+            w="40vw"
+            objectFit="contain"
+            src={croppedCaptureDataURL}
+            alt="Cropped capture"
+          />
         </Flex>
-        <Box w="40vw">
-          <ProfileCardPhoto imageSrc={imageSrc} />
-        </Box>
-      </Flex>
-    </Box>
+        <Flex direction="row" justify="end" gap="2vw">
+          <IconWithLabel icon={HiArrowCircleLeft} label="첫 화면" />
+          <IconWithLabel icon={GiButtonFinger} label="선택" />
+        </Flex>
+      </Container>
+    </Center>
   )
-
-  return <Layout heading="프로필 사진" body={body} />
-}
-
-const imageHandler = (imageSrc) => {
-  let img = new Image()
-  img.src = imageSrc
-  let width = img.width
-  let height = img.height
-  console.log(imageSrc)
-  console.log(width, height)
-}
-
-function cropImage(
-  imgUri,
-  width = 400,
-  height = 300,
-  xstart = 0,
-  ystart = 0,
-  callback
-) {
-  try {
-    console.log('Values in crop', width, height)
-    let resize_canvas = document.createElement('canvas')
-    let orig_src = new Image()
-    orig_src.src = imgUri
-    orig_src.onload = function () {
-      resize_canvas.width = width
-      resize_canvas.height = height
-      let cnv = resize_canvas.getContext('2d')
-      cnv.drawImage(
-        orig_src,
-        xstart,
-        ystart,
-        width,
-        height,
-        0,
-        0,
-        width,
-        height
-      )
-      let newimgUri = resize_canvas.toDataURL('image/png').toString()
-      callback(newimgUri)
-    }
-  } catch (e) {
-    console.log("Couldn't crop image due to", e)
-    window.alert("Couldn't crop image due to", e)
-    callback(imgUri)
-  }
 }
 
 export default AfterShootPage
