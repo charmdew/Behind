6d5ea12370434d54ecc3f1de43db976a958406d8
Joonha Yun <neverdcloud@gmail.com>
2023-02-11 17:39:05 +0900

    modify menu page

---
+++ behind-embedded-front/src/pages/MenuPage.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { Center, Flex } from '@chakra-ui/react'
+import { HiArrowCircleUp } from 'react-icons/hi'
+import { GiButtonFinger } from 'react-icons/gi'
+
+import Container from '../components/Container'
+import Header from '../components/Header'
+import ButtonLg from '../components/ButtonLg'
+import IconWithLabel from '../components/IconWithLabel'
+
+const MenuSelectPage = () => {
+  const { state } = useLocation()
+  const navigate = useNavigate()
+  const [selection, setSelection] = useState('left')
+
+  const keyDownHandler = (e) => {
+    if (e.key === 'ArrowUp')
+      navigate('/reset', { state: { prevPage: '/menu', ...state } })
+    if (e.key === 'ArrowLeft') setSelection('left')
+    if (e.key === 'ArrowRight') setSelection('right')
+    if (e.key === 'Enter') {
+      if (selection === 'left') navigate('/photo-shoot-guide', { state: state })
+      if (selection === 'right')
+        navigate('/photo-get-and-select', { state: state })
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDownHandler)
+    return () => window.removeEventListener('keydown', keyDownHandler)
+  }, [selection])
+
+  return (
+    <Center w="100vw" h="100vh">
+      <Container>
+        <Header>메뉴</Header>
+        <Flex as="main" direction="row" justify="center" gap="5vw" my="8vw">
+          <ButtonLg isSelected={selection === 'left'}>사진 촬영</ButtonLg>
+          <ButtonLg isSelected={selection === 'right'}>카드 출력</ButtonLg>
+        </Flex>
+        <Flex direction="row" justify="end" gap="2vw">
+          <IconWithLabel icon={HiArrowCircleUp} label="첫 화면" />
+          <IconWithLabel icon={GiButtonFinger} label="선택" />
+        </Flex>
+      </Container>
+    </Center>
+  )
+}
+
+export default MenuSelectPage
