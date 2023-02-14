3786552b141d7b4c178b4e10bce777e69635fdce
Joonha Yun <neverdcloud@gmail.com>
2023-02-14 23:59:00 +0900

    change profile card style

--- behind-embedded-front/src/components/ProfileCard.js
+++ behind-embedded-front/src/components/ProfileCard.js
@@ -5,48 +5,73 @@ import { FiAtSign } from 'react-icons/fi'
 
 const ProfileCard = (props) => {
   return (
-    <Box id={props.id} w="320px" h="512px" px="10px" bgColor="white">
+    <Box
+      id={props.id}
+      w={props.width + 'px'}
+      h={(1024 / 640) * props.width + 'px'}
+      px={(16 / 640) * props.width + 'px'}
+      py={(8 / 640) * props.width + 'px'}>
       <Box
-        borderWidth="0.5px"
+        borderWidth={(1 / 640) * props.width + 'px'}
         borderStyle="solid"
         borderColor="blackAlpha.300"
         borderRadius="2xl"
         overflow="hidden"
         bgColor="black">
-        <Image w="100%" h="380px" src={props.src} />
-        <Flex direction="column" p="10px">
-          <Flex direction="row" gap="10px" align="center">
+        <Image w="100%" h={(760 / 640) * props.width + 'px'} src={props.src} />
+        <Flex direction="column" p={(20 / 640) * props.width + 'px'}>
+          <Flex
+            direction="row"
+            gap={(20 / 640) * props.width + 'px'}
+            align="center">
             <Box
               as="span"
-              fontSize="25px"
+              fontSize={(50 / 640) * props.width + 'px'}
               fontWeight="extrabold"
               color="gray.200">
               {props.name}
             </Box>
-            <Box as="span" fontSize="15px" color="gray.500">
+            <Box
+              as="span"
+              fontSize={(30 / 640) * props.width + 'px'}
+              color="gray.500">
               {props.tagString}
             </Box>
           </Flex>
           <Flex direction="column">
-            <Flex direction="row" align="center" gap="2px">
-              <Icon as={FiAtSign} boxSize="20px" color="gray.300" />
+            <Flex
+              direction="row"
+              align="center"
+              gap={(4 / 640) * props.width + 'px'}>
+              <Icon
+                as={FiAtSign}
+                boxSize={(40 / 640) * props.width + 'px'}
+                color="gray.300"
+              />
               <Box
                 as="span"
-                m="3px"
+                m={(6 / 640) * props.width + 'px'}
                 fontStyle="italic"
-                fontSize="14px"
+                fontSize={(28 / 640) * props.width + 'px'}
                 fontWeight="semibold"
                 color="gray.300">
                 {props.email}
               </Box>
             </Flex>
-            <Flex direction="row" align="center" gap="2px">
-              <Icon as={BsPhone} boxSize="20px" color="gray.300" />
+            <Flex
+              direction="row"
+              align="center"
+              gap={(4 / 640) * props.width + 'px'}>
+              <Icon
+                as={BsPhone}
+                boxSize={(40 / 640) * props.width + 'px'}
+                color="gray.300"
+              />
               <Box
                 as="span"
-                m="3px"
+                m={(6 / 640) * props.width + 'px'}
                 fontStyle="italic"
-                fontSize="14px"
+                fontSize={(28 / 640) * props.width + 'px'}
                 fontWeight="semibold"
                 color="gray.300">
                 {props.phoneNum}
--- behind-embedded-front/src/pages/PrintPage.js
+++ behind-embedded-front/src/pages/PrintPage.js
@@ -5,7 +5,7 @@
 const os = require('node:os')
 import React, { useState, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { Center, Flex } from '@chakra-ui/react'
+import { Center, Flex, Box } from '@chakra-ui/react'
 import { FaArrowLeft, FaArrowsAltV } from 'react-icons/fa'
 import { GiButtonFinger } from 'react-icons/gi'
 import jwt_decode from 'jwt-decode'
@@ -34,7 +34,7 @@ const PrintPage = ({ socketClient }) => {
     }
     if (e.key === 'Enter') {
       if (selection === 1) {
-        const downloadPath = os.homedir() + '/image.jpg'
+        const downloadPath = os.homedir() + '/image.png'
         saveElToImage('profile-card', downloadPath, () => {
           socketClient.send('print')
           navigate('/end')
@@ -67,6 +67,11 @@ const PrintPage = ({ socketClient }) => {
   }
 
   useEffect(() => {
+    const body = document.querySelector('body')
+    body.style.overflow = 'hidden'
+  })
+
+  useEffect(() => {
     window.addEventListener('keydown', keyDownHandler)
     return () => window.removeEventListener('keydown', keyDownHandler)
   }, [selection])
@@ -76,31 +81,45 @@ const PrintPage = ({ socketClient }) => {
   }, [])
 
   return (
-    <Center w="100vw" h="100vh">
-      <Container>
-        <Header>출력</Header>
-        <Flex as="main" direction="row" justify="center" gap="5vw">
-          <Flex direction="column" justify="center" gap="2vw">
-            <ButtonLg isSelected={selection === 1}>출력하기</ButtonLg>
-            <ButtonLg isSelected={selection === 2}>다시 고르기</ButtonLg>
-            <ButtonLg isSelected={selection === 3}>정보 갱신</ButtonLg>
+    <>
+      <Center w="100vw" h="100vh">
+        <Container>
+          <Header>출력</Header>
+          <Flex as="main" direction="row" justify="center" gap="5vw">
+            <Flex direction="column" justify="center" gap="2vw">
+              <ButtonLg isSelected={selection === 1}>출력하기</ButtonLg>
+              <ButtonLg isSelected={selection === 2}>다시 고르기</ButtonLg>
+              <ButtonLg isSelected={selection === 3}>정보 갱신</ButtonLg>
+            </Flex>
+            <ProfileCard
+              id="profile-card-scaled"
+              name={userInfo.name}
+              tagString={userInfo.tagString}
+              email={userInfo.email}
+              phoneNum={userInfo.phoneNum}
+              src={state.profileImageDataURL}
+              width={320}
+            />
           </Flex>
-          <ProfileCard
-            id="profile-card"
-            name={userInfo.name}
-            tagString={userInfo.tagString}
-            email={userInfo.email}
-            phoneNum={userInfo.phoneNum}
-            src={state.profileImageDataURL}
-          />
-        </Flex>
-        <Flex direction="row" justify="end" gap="2vw">
-          <IconWithLabel icon={FaArrowLeft} label="첫 화면" />
-          <IconWithLabel icon={FaArrowsAltV} label="이동" />
-          <IconWithLabel icon={GiButtonFinger} label="선택" />
-        </Flex>
-      </Container>
-    </Center>
+          <Flex direction="row" justify="end" gap="2vw">
+            <IconWithLabel icon={FaArrowLeft} label="첫 화면" />
+            <IconWithLabel icon={FaArrowsAltV} label="이동" />
+            <IconWithLabel icon={GiButtonFinger} label="선택" />
+          </Flex>
+        </Container>
+      </Center>
+      <Box>
+        <ProfileCard
+          id="profile-card"
+          name={userInfo.name}
+          tagString={userInfo.tagString}
+          email={userInfo.email}
+          phoneNum={userInfo.phoneNum}
+          src={state.profileImageDataURL}
+          width={640}
+        />
+      </Box>
+    </>
   )
 }
 
