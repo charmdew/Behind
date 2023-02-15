d05d8027a912a9b632137975dd343e892a457a20
이상훈 <akqjqdmltn@naver.com>
2023-02-15 22:43:41 +0900

    Update behind-back/src/main/java/com/reboot/behind/config/security/oauth/OAuth2SuccessHandler.java

--- behind-back/src/main/java/com/reboot/behind/config/security/oauth/OAuth2SuccessHandler.java
+++ behind-back/src/main/java/com/reboot/behind/config/security/oauth/OAuth2SuccessHandler.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import com.reboot.behind.service.userService;
+import com.reboot.behind.service.UserService;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
