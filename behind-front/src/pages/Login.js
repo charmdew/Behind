133d1860b813f4fc1ec198c12711bef09f8c8cba
유정훈 <choanury@naver.com>
2023-02-15 09:42:46 +0900

    login button

--- behind-front/src/pages/Login.js
+++ behind-front/src/pages/Login.js
@@ -10,7 +10,11 @@ const Login = () => {
           http://ec2-13-209-17-196.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/kakao"
         >
           <Image
-            w="70vw"
+            // w="70vw"
+            w={{
+              base: '60vw',
+              lg: '30vw',
+            }}
             mt="10"
             src="images/kakao_login_large_narrow.png"
             borderRadius="12"
@@ -19,7 +23,10 @@ const Login = () => {
         </a>
         <a mt="10" href="api/oauth2/authorization/google">
           <Image
-            w="72vw"
+            w={{
+              base: '62vw',
+              lg: '31vw',
+            }}
             mt="2"
             src="images/btn_google_signin_light_normal_web@2x.png"
           ></Image>
