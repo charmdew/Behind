5588f29a50def5c13fe80a286239758fe99e8f48
Joonha Yun <neverdcloud@gmail.com>
2023-02-17 10:24:08 +0900

    change start page style

--- behind-embedded-front/src/pages/StartPage.js
+++ behind-embedded-front/src/pages/StartPage.js
@@ -4,7 +4,8 @@
 
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Center, Box, Flex } from '@chakra-ui/react'
+import { Center, Box, Flex, Image } from '@chakra-ui/react'
+import logo from '../assets/logo.png'
 
 const StartPage = () => {
   const navigate = useNavigate()
@@ -20,11 +21,9 @@ const StartPage = () => {
 
   return (
     <Center w="100vw" h="100vh">
-      <Flex direction="column">
-        <Box fontSize="15vw" color="white" textAlign="center">
-          Behind
-        </Box>
-        <Box fontSize="5vw" color="white">
+      <Flex w="80vw" direction="column">
+        <Image src={logo} alt="Logo" />
+        <Box fontSize="5vw" color="white" textAlign="center">
           가운데 버튼을 클릭해주세요
         </Box>
       </Flex>
